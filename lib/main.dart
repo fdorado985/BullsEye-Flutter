e8859ec5d60779b764e031da6b03f3f457bea0f9
Juan Dorado <fdorado985@gmail.com>
2020-12-16 21:21:49 -0600

    Create the initial entry with an empty container

---
+++ lib/main.dart
@@ -0,0 +1,14 @@
+import 'package:flutter/material.dart';
+
+void main() => runApp(BullsEyeApp());
+
+class BullsEyeApp extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      title: 'BullsEye',
+      theme: ThemeData(primarySwatch: Colors.blue),
+      home: Container(),
+    );
+  }
+}
