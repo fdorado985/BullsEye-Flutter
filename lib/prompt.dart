8f20a64786897b80f8c3db43fdf620f563e3cc31
Juan Dorado <fdorado985@gmail.com>
2020-12-17 00:37:43 -0600

    Create the prompt on a new file

---
+++ lib/prompt.dart
@@ -0,0 +1,17 @@
+import 'package:flutter/material.dart';
+
+class Prompt extends StatelessWidget {
+  Prompt({@required this.targetValue});
+
+  final int targetValue;
+
+  @override
+  Widget build(BuildContext context) {
+    return Column(
+      children: [
+        Text('PUT THE BULLSEYE AS CLOSE AS YOU CAN TO'),
+        Text('$targetValue'),
+      ],
+    );
+  }
+}
