40fff59bda4ecd0100e293abc7b7424bae3e6240
Juan Dorado <fdorado985@gmail.com>
2020-12-17 21:58:43 -0600

    Create score file to improve the challenge

---
+++ lib/score.dart
@@ -0,0 +1,17 @@
+import 'package:flutter/material.dart';
+
+class Score extends StatelessWidget {
+  Score({
+    Key key,
+    @required this.totalScore,
+    @required this.round,
+  }) : super(key: key);
+
+  final int totalScore;
+  final int round;
+
+  @override
+  Widget build(BuildContext context) {
+    return Container();
+  }
+}
